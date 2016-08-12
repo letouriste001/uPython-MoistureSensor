802b177bfdff1ad8257b635d0dc061c660b658fd
Christophe AUBERT <aubert.christophe.pro@gmail.com>
2016-08-12 11:00:33 +0200

    Etalonage du capteur

--- MoistureSensor.py
+++ MoistureSensor.py
@@ -3,6 +3,15 @@ import pyb
 class MoistureSensor:
     
     
-    def __init__(self):
-        self.value : 0
-        self.pin = pyb.Pin
+    def __init__(self, pinADC="X11"):
+        self.value = 0
+        self.pin = pyb.ADC(pinADC)
+        
+    def measure(self):
+        
+        if(self.pin.read() > 1700):
+            self.value = 100
+        else:
+            self.value = self.pin.read() * 100 / 1700
+        print("Raw : " + str(self.pin.read()))
+        return self.value
--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 import pyb
+from MoistureSensor import MoistureSensor
+
+print("Start program test moisture sensor")
+
+sensor = MoistureSensor()
+
+while True:
+    print("humidity : " + str(sensor.measure()))
+    pyb.delay(1000)
+
